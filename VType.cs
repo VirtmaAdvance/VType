342875ac455135cbfbc83f9d2e2f116ad99c9f2d
WarmasterPrime <warmasterprime.gamming@gmail.com>
2023-10-25 08:08:57 -1000

    - Removed PrimaryType field.

--- VType.cs
+++ VType.cs
@@ -11,76 +11,54 @@ namespace VType
 	/// </summary>
 	public class VType : Type
 	{
-
-		private object _value = Activator.CreateInstance(typeof(object))!;
 		/// <summary>
 		/// The instance of the object.
 		/// </summary>
-		public object Value
-		{
-			get => _value;
-			set
-			{
-				_value=value;
-				PrimaryType=(value is not null) ? (System.Runtime.InteropServices.Marshal.IsComObject(value) ? null : value?.GetType()) : null;
-				if(PrimaryType is not null)
-				{
-					Members=PrimaryType.GetMembers();
-					Fields=PrimaryType.GetFields();
-					Methods=PrimaryType.GetMethods();
-					Constructors=PrimaryType.GetConstructors();
-					Properties=PrimaryType.GetProperties();
-				}
-			}
-		}
-		/// <summary>
-		/// The primary <see cref="Type"/> object.
-		/// </summary>
-		public Type? PrimaryType { get; private set; } = null;
+		public object? Value { get; set; } = null;
 		/// <summary>
 		/// Indicates whether the value is <see langword="null"/>.
 		/// </summary>
-		public bool IsNull => PrimaryType is null;
+		public bool IsNull => this is null;
 		/// <summary>
 		/// Indicates whether the value is not <see langword="null"/>.
 		/// </summary>
-		public bool NotNull => PrimaryType is not null;
+		public bool NotNull => this is not null;
 		/// <summary>
 		/// Indicates whether the value is a COMObject.
 		/// </summary>
-		public bool IsComObject => NotNull && PrimaryType!.IsCOMObject;
+		public bool IsComObject => NotNull && IsCOMObject;
 		/// <inheritdoc cref="Type.Assembly"/>
-		public override Assembly Assembly => NotNull ? PrimaryType!.Assembly : throw new InvalidOperationException();
+		public override Assembly Assembly => NotNull ? Assembly : throw new InvalidOperationException();
 		/// <inheritdoc cref="Type.AssemblyQualifiedName"/>
-		public override string? AssemblyQualifiedName => PrimaryType?.AssemblyQualifiedName;
+		public override string? AssemblyQualifiedName => AssemblyQualifiedName;
 		/// <inheritdoc cref="Type.BaseType"/>
-		public override Type? BaseType => PrimaryType?.BaseType;
+		public override Type? BaseType => BaseType;
 		/// <inheritdoc cref="Type.FullName"/>
-		public override string? FullName => PrimaryType?.FullName;
+		public override string? FullName => FullName;
 		/// <inheritdoc cref="Type.GUID"/>
-		public override Guid GUID => NotNull ? PrimaryType!.GUID : Guid.Empty;
+		public override Guid GUID => NotNull ? GUID : Guid.Empty;
 		/// <inheritdoc cref="Type.Module"/>
-		public override Module Module => NotNull ? PrimaryType!.Module : throw new InvalidOperationException();
+		public override Module Module => NotNull ? Module : throw new InvalidOperationException();
 		/// <inheritdoc cref="Type.Namespace"/>
-		public override string? Namespace => PrimaryType?.Namespace;
+		public override string? Namespace => Namespace;
 		/// <inheritdoc cref="Type.UnderlyingSystemType"/>
-		public override Type UnderlyingSystemType => NotNull ? PrimaryType!.UnderlyingSystemType : throw new InvalidOperationException();
+		public override Type UnderlyingSystemType => NotNull ? UnderlyingSystemType : throw new InvalidOperationException();
 		/// <summary>
 		/// The name of the object data-type.
 		/// </summary>
-		public override string Name => NotNull ? PrimaryType!.Name : "null";
+		public override string Name => NotNull ? Name : "null";
 		/// <summary>
 		/// Indicates whether the value is iterable.
 		/// </summary>
-		public bool IsIterable => (NotNull) && (PrimaryType!.IsAssignableFrom(typeof(IEnumerable)) || PrimaryType!.IsAssignableFrom(typeof(Array)) || ((PrimaryType!.IsClass || !PrimaryType!.IsArray) && Properties.FirstOrDefault(q=>q.Name=="IsArray" || q.Name=="IsIterable") is not null));
+		public bool IsIterable => (NotNull) && (IsAssignableFrom(typeof(IEnumerable)) || IsAssignableFrom(typeof(Array)) || ((IsClass || !IsArray) && Properties.FirstOrDefault(q=>q.Name=="IsArray" || q.Name=="IsIterable") is not null));
 		/// <summary>
 		/// Indicates whether the object inherits from the <see cref="IEnumerable"/> interface.
 		/// </summary>
-		public bool IsEnumerable => NotNull && PrimaryType!.IsAssignableFrom(typeof(IEnumerable));
+		public bool IsEnumerable => NotNull && IsAssignableFrom(typeof(IEnumerable));
 		/// <summary>
 		/// Indicates whether the object inherits from the <see cref="ICollection"/> interface.
 		/// </summary>
-		public bool IsCollection => NotNull && PrimaryType!.IsAssignableFrom(typeof(ICollection));
+		public bool IsCollection => NotNull && IsAssignableFrom(typeof(ICollection));
 		/// <summary>
 		/// Determines if the value inherits from any numerical data-type or has a field "IsNumber" set to <see cref="bool">true</see>.
 		/// </summary>
@@ -88,23 +66,23 @@ namespace VType
 		/// <summary>
 		/// The object's members.
 		/// </summary>
-		public MemberInfo[] Members { get; private set; } = Array.Empty<MemberInfo>();
+		public MemberInfo[] Members => NotNull ? GetMembers() : Array.Empty<MemberInfo>();
 		/// <summary>
 		/// The object's fields.
 		/// </summary>
-		public FieldInfo[] Fields { get; private set; } = Array.Empty<FieldInfo>();
+		public FieldInfo[] Fields => NotNull ? GetFields() : Array.Empty<FieldInfo>();
 		/// <summary>
 		/// The object's methods.
 		/// </summary>
-		public MethodInfo[] Methods { get; private set; } = Array.Empty<MethodInfo>();
+		public MethodInfo[] Methods => NotNull ? GetMethods() : Array.Empty<MethodInfo>();
 		/// <summary>
 		/// The object's constructors.
 		/// </summary>
-		public ConstructorInfo[] Constructors { get; private set; } = Array.Empty<ConstructorInfo>();
+		public ConstructorInfo[] Constructors => NotNull ? GetConstructors() : Array.Empty<ConstructorInfo>();
 		/// <summary>
 		/// The object's properties.
 		/// </summary>
-		public PropertyInfo[] Properties { get; private set; } = Array.Empty<PropertyInfo>();
+		public PropertyInfo[] Properties => NotNull ? GetProperties() : Array.Empty<PropertyInfo>();
 
 
 		/// <summary>
@@ -138,24 +116,24 @@ namespace VType
 		/// <returns></returns>
 		protected override ConstructorInfo? GetConstructorImpl(BindingFlags bindingAttr, Binder? binder, CallingConventions callConvention, Type[] types, ParameterModifier[]? modifiers) => GetConstructor(bindingAttr, binder, callConvention, types, modifiers);
 		/// <inheritdoc cref="Type.GetConstructors(BindingFlags)"/>
-		public override ConstructorInfo[] GetConstructors(BindingFlags bindingAttr) => NotNull ? PrimaryType!.GetConstructors(bindingAttr) : Array.Empty<ConstructorInfo>();
+		public override ConstructorInfo[] GetConstructors(BindingFlags bindingAttr) => NotNull ? GetConstructors(bindingAttr) : Array.Empty<ConstructorInfo>();
 		/// <inheritdoc cref="Type.GetElementType()"/>
-		public override Type? GetElementType() => PrimaryType?.GetElementType();
+		public override Type? GetElementType() => GetElementType();
 		/// <inheritdoc cref="Type.GetEvent(string, BindingFlags)"/>
-		public override EventInfo? GetEvent(string name, BindingFlags bindingAttr) => PrimaryType?.GetEvent(name, bindingAttr);
+		public override EventInfo? GetEvent(string name, BindingFlags bindingAttr) => GetEvent(name, bindingAttr);
 		/// <inheritdoc cref="Type.GetEvents(BindingFlags)"/>
-		public override EventInfo[] GetEvents(BindingFlags bindingAttr) => NotNull ? PrimaryType!.GetEvents(bindingAttr) : Array.Empty<EventInfo>();
+		public override EventInfo[] GetEvents(BindingFlags bindingAttr) => NotNull ? GetEvents(bindingAttr) : Array.Empty<EventInfo>();
 		/// <inheritdoc cref="Type.GetField(string, BindingFlags)"/>
-		public override FieldInfo? GetField(string name, BindingFlags bindingAttr) => PrimaryType?.GetField(name, bindingAttr);
+		public override FieldInfo? GetField(string name, BindingFlags bindingAttr) => GetField(name, bindingAttr);
 		/// <inheritdoc cref="Type.GetFields(BindingFlags)"/>
-		public override FieldInfo[] GetFields(BindingFlags bindingAttr) => NotNull ? PrimaryType!.GetFields(bindingAttr) : Array.Empty<FieldInfo>();
+		public override FieldInfo[] GetFields(BindingFlags bindingAttr) => NotNull ? GetFields(bindingAttr) : Array.Empty<FieldInfo>();
 		/// <inheritdoc cref="Type.GetInterface(string, bool)"/>
 		[return: DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.Interfaces)]
-		public override Type? GetInterface(string name, bool ignoreCase) => PrimaryType?.GetInterface(name, ignoreCase);
+		public override Type? GetInterface(string name, bool ignoreCase) => GetInterface(name, ignoreCase);
 		/// <inheritdoc cref="Type.GetInterfaces()"/>
-		public override Type[] GetInterfaces() => NotNull ? PrimaryType!.GetInterfaces() : Array.Empty<Type>();
+		public override Type[] GetInterfaces() => NotNull ? GetInterfaces() : Array.Empty<Type>();
 		/// <inheritdoc cref="Type.GetMembers(BindingFlags)"/>
-		public override MemberInfo[] GetMembers(BindingFlags bindingAttr) => NotNull ? PrimaryType!.GetMembers(bindingAttr) : Array.Empty<MemberInfo>();
+		public override MemberInfo[] GetMembers(BindingFlags bindingAttr) => NotNull ? GetMembers(bindingAttr) : Array.Empty<MemberInfo>();
 		/// <summary>
 		/// 
 		/// </summary>
@@ -166,15 +144,15 @@ namespace VType
 		/// <param name="types"></param>
 		/// <param name="modifiers"></param>
 		/// <returns></returns>
-		protected override MethodInfo? GetMethodImpl(string name, BindingFlags bindingAttr, Binder? binder, CallingConventions callConvention, Type[]? types, ParameterModifier[]? modifiers) => PrimaryType?.GetMethod(name, bindingAttr, binder, callConvention, types??Array.Empty<Type>(), modifiers);
+		protected override MethodInfo? GetMethodImpl(string name, BindingFlags bindingAttr, Binder? binder, CallingConventions callConvention, Type[]? types, ParameterModifier[]? modifiers) => GetMethod(name, bindingAttr, binder, callConvention, types??Array.Empty<Type>(), modifiers);
 		/// <inheritdoc cref="Type.GetMethods(BindingFlags)"/>
-		public override MethodInfo[] GetMethods(BindingFlags bindingAttr) => NotNull ? PrimaryType!.GetMethods(bindingAttr) : Array.Empty<MethodInfo>();
+		public override MethodInfo[] GetMethods(BindingFlags bindingAttr) => NotNull ? GetMethods(bindingAttr) : Array.Empty<MethodInfo>();
 		/// <inheritdoc cref="Type.GetNestedType(string, BindingFlags)"/>
-		public override Type? GetNestedType(string name, BindingFlags bindingAttr) => PrimaryType?.GetNestedType(name, bindingAttr);
+		public override Type? GetNestedType(string name, BindingFlags bindingAttr) => GetNestedType(name, bindingAttr);
 		/// <inheritdoc cref="Type.GetNestedTypes(BindingFlags)"/>
-		public override Type[] GetNestedTypes(BindingFlags bindingAttr) => NotNull ? PrimaryType!.GetNestedTypes(bindingAttr) : Array.Empty<Type>();
+		public override Type[] GetNestedTypes(BindingFlags bindingAttr) => NotNull ? GetNestedTypes(bindingAttr) : Array.Empty<Type>();
 		/// <inheritdoc cref="Type.GetProperties(BindingFlags)"/>
-		public override PropertyInfo[] GetProperties(BindingFlags bindingAttr) => NotNull ? PrimaryType!.GetProperties(bindingAttr) : Array.Empty<PropertyInfo>();
+		public override PropertyInfo[] GetProperties(BindingFlags bindingAttr) => NotNull ? GetProperties(bindingAttr) : Array.Empty<PropertyInfo>();
 		/// <summary>
 		/// 
 		/// </summary>
@@ -186,55 +164,55 @@ namespace VType
 		/// <param name="modifiers"></param>
 		/// <returns></returns>
 		/// <exception cref="NotImplementedException"></exception>
-		protected override PropertyInfo? GetPropertyImpl(string name, BindingFlags bindingAttr, Binder? binder, Type? returnType, Type[]? types, ParameterModifier[]? modifiers) => PrimaryType?.GetProperty(name, bindingAttr, binder, returnType, types!, modifiers);
+		protected override PropertyInfo? GetPropertyImpl(string name, BindingFlags bindingAttr, Binder? binder, Type? returnType, Type[]? types, ParameterModifier[]? modifiers) => GetProperty(name, bindingAttr, binder, returnType, types!, modifiers);
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <returns></returns>
-		protected override bool HasElementTypeImpl() => NotNull ? PrimaryType!.HasElementType : false;
+		protected override bool HasElementTypeImpl() => NotNull ? HasElementType : false;
 		/// <inheritdoc cref="Type.InvokeMember(string, BindingFlags, Binder, object, object[], ParameterModifier[], CultureInfo, string[])"/>
-		public override object? InvokeMember(string name, BindingFlags invokeAttr, Binder? binder, object? target, object?[]? args, ParameterModifier[]? modifiers, CultureInfo? culture, string[]? namedParameters) => PrimaryType?.InvokeMember(name, invokeAttr, binder, target, args, modifiers, culture, namedParameters);
+		public override object? InvokeMember(string name, BindingFlags invokeAttr, Binder? binder, object? target, object?[]? args, ParameterModifier[]? modifiers, CultureInfo? culture, string[]? namedParameters) => InvokeMember(name, invokeAttr, binder, target, args, modifiers, culture, namedParameters);
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <returns></returns>
 		/// <exception cref="NotImplementedException"></exception>
-		protected override bool IsArrayImpl() => NotNull ? PrimaryType!.IsArray : false;
+		protected override bool IsArrayImpl() => NotNull ? IsArray : false;
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <returns></returns>
 		/// <exception cref="NotImplementedException"></exception>
-		protected override bool IsByRefImpl() => NotNull ? PrimaryType!.IsByRef : false;
+		protected override bool IsByRefImpl() => NotNull ? IsByRef : false;
 		/// <summary>
 		/// Determines if the value is a COMObject.
 		/// </summary>
 		/// <returns></returns>
 		/// <exception cref="NotImplementedException"></exception>
-		protected override bool IsCOMObjectImpl() => NotNull ? PrimaryType!.IsCOMObject : false;
+		protected override bool IsCOMObjectImpl() => NotNull ? IsCOMObject : false;
 		/// <summary>
 		/// Determines if the value is a pointer reference.
 		/// </summary>
 		/// <returns></returns>
-		protected override bool IsPointerImpl() => NotNull ? PrimaryType!.IsPointer : false;
+		protected override bool IsPointerImpl() => NotNull ? IsPointer : false;
 		/// <summary>
 		/// Determines if the value is primitive.
 		/// </summary>
 		/// <returns></returns>
-		protected override bool IsPrimitiveImpl() => NotNull ? PrimaryType!.IsPrimitive : false;
+		protected override bool IsPrimitiveImpl() => NotNull ? IsPrimitive : false;
 		/// <summary>
 		/// Gets an array of custom attributes.
 		/// </summary>
 		/// <param name="inherit"></param>
 		/// <returns></returns>
-		public override object[] GetCustomAttributes(bool inherit) => NotNull ? PrimaryType!.GetCustomAttributes(inherit) : Array.Empty<object>();
+		public override object[] GetCustomAttributes(bool inherit) => NotNull ? GetCustomAttributes(inherit) : Array.Empty<object>();
 		/// <summary>
 		/// Gets an array of custom attributes.
 		/// </summary>
 		/// <param name="attributeType"></param>
 		/// <param name="inherit"></param>
 		/// <returns></returns>
-		public override object[] GetCustomAttributes(Type attributeType, bool inherit) => NotNull ? PrimaryType!.GetCustomAttributes(attributeType, inherit) : Array.Empty<object>();
+		public override object[] GetCustomAttributes(Type attributeType, bool inherit) => NotNull ? GetCustomAttributes(attributeType, inherit) : Array.Empty<object>();
 		/// <summary>
 		/// Determines if this object is defined.
 		/// </summary>
@@ -242,7 +220,7 @@ namespace VType
 		/// <param name="inherit"></param>
 		/// <returns></returns>
 		/// <exception cref="NotImplementedException"></exception>
-		public override bool IsDefined(Type attributeType, bool inherit) => NotNull ? PrimaryType!.IsDefined(attributeType, inherit) : false;
+		public override bool IsDefined(Type attributeType, bool inherit) => NotNull ? IsDefined(attributeType, inherit) : false;
 		/// <summary>
 		/// Determines if the value can inherit from any of the given <paramref name="types"/>.
 		/// </summary>
